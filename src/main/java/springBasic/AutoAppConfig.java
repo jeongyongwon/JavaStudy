635a295092292d88ee2bcf554578e32b1baeeafd
정용원 <yongdev>
2023-02-25 19:48:49 +0900

    자바 최척화:jvm intro

--- src/main/java/springBasic/AutoAppConfig.java
+++ src/main/java/springBasic/AutoAppConfig.java
@@ -3,11 +3,15 @@ package springBasic;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 
-//Configuration이 붙은 annotation이 붙은 bean 객체 주입하는 클래스들을 필터링한다
-//ComponentScan 과 충돌하지 않기 위해
+
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+//Configuration annotation이  붙은 bean 객체 주입하는 클래스들이 ComponentScan 과 충돌하지 않기 위해 필터링한다
+//@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+
+//패키지 단위로 설정할 수 있다
+@ComponentScan(
+        basePackages = {"springBasic"}
+)
 public class AutoAppConfig {
 }
