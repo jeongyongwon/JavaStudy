956e61710d4515494728dbb1ba7707f9c79d561a
정용원 <yongdev>
2023-02-25 22:12:26 +0900

    인터페이스로 빈 생명주기 조절

---
+++ src/main/java/springBasic/beanlifecycle/App.java
@@ -0,0 +1,27 @@
+package springBasic.beanlifecycle;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ac = new
+                AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close(); //스프링 컨테이너를 종료, ConfigurableApplicationContext 필요
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
